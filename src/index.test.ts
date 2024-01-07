c921736ff95eb920acbde29c22fc07eacffe3208
Jayant Malik <dev.jayantmalik@gmail.com>
2024-01-07 13:36:11 +0530

    added release and fixed tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,4 +3,5 @@ import { Version } from './index.js';
 
 t.test('Version response should follow format MAJOR.MINOR.PATCH', (t) => {
   t.match(Version(), /^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}$/);
+  t.end();
 });
