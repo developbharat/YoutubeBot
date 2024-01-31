176aeabeb9b00a4a5fc01b938b11dc1f5b63deca
Jayant Malik <dev.jayantmalik@gmail.com>
2024-01-31 18:35:23 +0530

    added types export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export * from './protocols/index.js';
+export {
+  IAPIDetails,
+  IChannelAboutCombinedDetails,
+  IThumbnail,
+  IChannelAboutDetails,
+  IChannelMetadata
+} from './DataClasses/index.js';
